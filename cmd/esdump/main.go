34d4119d94a769e4ae3bdd129ff7505b6cdcbbd0
Martin Czygan <martin.czygan@gmail.com>
2020-04-23 21:58:14 +0200

    fix off-by-one error where last batch would be ignored

--- cmd/esdump/main.go
+++ cmd/esdump/main.go
@@ -171,7 +171,7 @@ func (s *BasicScroller) Next() bool {
 	if len(sr.Hits.Hits) == 0 && int64(s.total) != sr.Hits.Total {
 		log.Printf("warn: partial result")
 	}
-	return len(sr.Hits.Hits) > 0 && int64(s.total) < sr.Hits.Total
+	return len(sr.Hits.Hits) > 0 && int64(s.total) <= sr.Hits.Total
 }
 
 // Bytes returns the current response body.
